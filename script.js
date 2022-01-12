c6db4aa1f2466583c135681fc5bef4670a4481b7
robroskie <lroblesky@gmail.com>
2022-01-11 21:23:37 -0800

    right collision points incorrect

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
-let tiles = document.querySelectorAll("#tilemaster");
-
-let grid = [];
+const grid = [];
 for (let i = 0; i < 100; i++) {
   grid[i] = 0;
 }
 
-colorTestSquares = (ele1, ele2) => {
-  let count = 0;
-  tiles.forEach((t) => {
-    t.innerHTML = grid[count];
-
-    if (count == ele1 || count == ele2) {
-      t.style.color = "magenta";
-    } else {
-      t.style.color = "pink";
-    }
-    count++;
-  });
-};
-
 removeColor = (point) => {
-  // console.log(`removing point ${point}`);
   grid[point] = 0;
-
   let row = Math.floor(point / 10);
   let col = (point % 10) + 1;
   const temp_add = ".grid-item" + (row + 1) + "-" + col;
@@ -30,7 +12,6 @@ removeColor = (point) => {
 };
 
 addColorHandler = () => {
-  // console.log(`grid before adding colors ${grid}`);
   for (let i = 0; i < grid.length; i++) {
     if (grid[i] == 1) {
       addColor(i);
@@ -52,33 +33,12 @@ addColor = (point) => {
 };
 
 updateColors = (Obj) => {
-  //  console.log('call');
-  //  console.log(`arr${Obj.points} ${Obj.location}`)
   const arr = Obj.points;
 
-  // Obj.points.forEach(function (part, index, arr){
   for (let z = 0; z < arr.length; z++) {
     for (let i = 0; i < arr[z].length; i++) {
-      // console.log('element' + arr[z][i]);
       if (arr[z][i] == 1) {
         addColor(Obj.location + z * 10 + i);
-        // switch (z) {
-        //   case 0:
-        //     addColor(Obj.location + i);
-        //     break;
-        //   case 1:
-        //     addColor(Obj.location + 10 + i);
-        //     break;
-        //   case 2:
-        //     addColor(Obj.location + 20 + i);
-        //     break;
-        //     case 3:
-        //       addColor(Obj.location + 30 + i);
-        //       break;
-        //   default:
-        //     break;
-          // code block
-        // }
       }
     }
   }
@@ -86,86 +46,32 @@ updateColors = (Obj) => {
 
 removeAllColors = (Obj) => {
   const arr = Obj.points;
-  // Obj.points.forEach(function (part, index, arr){
+
   for (let z = 0; z < arr.length; z++) {
     for (let i = 0; i < arr[z].length; i++) {
-      // console.log("element" + arr[z][i]);
       if (arr[z][i] == 1) {
         const toRemove = Obj.location + z * 10 + i;
-        // console.log(`to remove=${toRemove}`);
+
         removeColor(toRemove);
       }
     }
   }
 };
 
-addShapeColors = (Obj) => {
-  //  console.log('call');
-  //  console.log(`arr${Obj.points} ${Obj.location}`)
-  const arr = Obj.points;
-  // Obj.points.forEach(function (part, index, arr){
-  for (let z = 0; z < arr.length; z++) {
-    for (let i = 0; i < arr[z].length; i++) {
-      console.log('addcolor to' + Obj.location + z * 10 + i);
-      if (arr[z][i] == 1) {
-        addColor(Obj.location + z * 10 + i);
-        // switch (z) {
-        //   case 0:
-        //     addColor(Obj.location + i);
-        //     break;
-        //   case 1:
-        //     addColor(Obj.location + 10 + i);
-        //     break;
-        //   case 2:
-        //     addColor(Obj.location + 20 + i);
-        //   case 3:
-        //     addColor(Obj.location + 30 + i);
-        //   default:
-        //     break;
-        //   // code block
-      }
-    }
-  }
-};
-
 checkCollisionPoints = (Obj) => {
-  const collisionPoints = Obj.getCollisionPoints();
-  console.log(collisionPoints);
-  // if (Obj instanceof Square) {
-  console.log("check");
+  // const collisionPoints = Obj.getCollisionPoints();
+  const collisionPoints = getBottomCollisionPoints(Obj);
   collisionPoints.forEach(function (part, index, arr) {
     const pointToCheck = Obj.location + arr[index][0] * 10 + arr[index][1];
-    console.log(
-      `Obj.location=${Obj.location}    arr[index][0] * 10 = ${
-        arr[index][0] * 10
-      }  arr[index][1] = ${arr[index][1]}  pointToCheck = ${pointToCheck}`
-    );
+
     if (grid[pointToCheck + 10] == 1 || pointToCheck >= 90) {
-      console.log("freeeeze");
       Obj.active = false;
     }
   });
 };
 
-// checkShapeFreeze = (Obj) => {
-//   const collisionPoints = Obj.getCollisionPoints();
-//   collisionPoints.forEach(function (part, index, arr) {
-//     // console.log(`checking ${Obj.location }`)
-//     const pointToCheck = Obj.location + arr[index][0] * 10 + arr[index][1];
-//     // console.log(`point to check is: ${pointToCheck}`);
-//     if (grid[pointToCheck + 10] == 1 || pointToCheck >= 90) {
-//       console.log('collision');
-//       Obj.active = false;
-//       // checkCompleteRows(Obj);
-//     }
-//   });
-// };
-
 moveShapeDown = (Obj) => {
-  console.log(Obj.points);
-  // checkShapeFreeze(Obj);
   if (Obj.active) {
-    // console.log("still active");
     removeAllColors(Obj);
 
     Obj.location += 10;
@@ -175,19 +81,94 @@ moveShapeDown = (Obj) => {
 };
 
 moveShapeLeft = (Obj) => {
-  Obj.location -= 1;
-  removeAllColors(Obj);
+  const leftColPoints = getLeftCollisionPoints(Obj);
 
-  updateColors(Obj);
+  let blocked = false;
+  leftColPoints.forEach(function (part, index, arr) {
+    const pointToCheck = Obj.location + arr[index][0] * 10 + arr[index][1];
 
-  // Obj.addColors();
+    if (grid[pointToCheck - 1] == 1 || pointToCheck % 10 == 0) {
+      blocked = true;
+    }
+  });
+
+  if (!blocked) {
+    Obj.location -= 1;
+    removeAllColors(Obj);
+
+    updateColors(Obj);
+  }
 };
 
 moveShapeRight = (Obj) => {
-  Obj.location += 1;
-  removeAllColors(Obj);
+  const rightColPoints = getLeftCollisionPoints(Obj);
+  console.log(rightColPoints);
+  let blocked = false;
+  rightColPoints.forEach(function (part, index, arr) {
+    const pointToCheck = Obj.location + arr[index][0] * 10 + arr[index][1];
 
-  updateColors(Obj);
+    if (grid[pointToCheck + 1] == 1 || pointToCheck % 10 == 9) {
+      blocked = true;
+    }
+  });
+  if (!blocked) {
+    getRightCollisionPoints(Obj);
+    Obj.location += 1;
+    removeAllColors(Obj);
+
+    updateColors(Obj);
+  }
+};
+
+getLeftCollisionPoints = (Shape) => {
+  const left_points = [];
+
+  console.log(Shape.points.length);
+  for (let i = 0; i < Shape.points.length; i++) {
+    let found = false;
+    for (let z = 0; z < Shape.points[i].length && !found; z++) {
+      if (Shape.points[i][z] == 1) {
+        left_points.push([i, z]);
+        found = true;
+      }
+    }
+  }
+
+  return left_points;
+};
+
+getRightCollisionPoints = (Shape) => {
+  const right_points = [];
+
+  console.log(Shape.points.length);
+  for (let i = 0; i < Shape.points.length; i++) {
+    let found = false;
+    for (let z = Shape.points[i].length; z >= 0 && !found; z--) {
+      if (Shape.points[i][z] == 1) {
+        console.log("pushing i, z=" + i + z);
+        right_points.push([i, z]);
+        found = true;
+      }
+    }
+  }
+
+  return right_points;
+};
+
+getBottomCollisionPoints = (Shape) => {
+  const bottom_points = [];
+
+  for (let i = Shape.points.length - 1; i >= 0; i--) {
+    let found = false;
+    for (let z = Shape.points[i].length - 1; z >= 0 && !found; z--) {
+      if (Shape.points[z][i] == 1) {
+        bottom_points.push([z, i]);
+        found = true;
+      }
+    }
+  }
+
+  return bottom_points;
 };
 
 function testBlock() {
@@ -200,24 +181,20 @@ function testBlock() {
 
   this.orientation = 0;
 
-  this.getCollisionPoints = function () {
-    return [
-      [1, 0],
-      [1, 1],
-      [1, 2],
-      [1, 3],
-      [1, 4],
-      [1, 5],
-      [1, 6],
-      [1, 7],
-      [1, 8],
-      [1, 9],
-    ];
-  };
-
-
-
-
+  // this.getCollisionPoints = function () {
+  //   return [
+  //     [1, 0],
+  //     [1, 1],
+  //     [1, 2],
+  //     [1, 3],
+  //     [1, 4],
+  //     [1, 5],
+  //     [1, 6],
+  //     [1, 7],
+  //     [1, 8],
+  //     [1, 9],
+  //   ];
+  // };
 
   this.active = true;
 }
@@ -234,23 +211,18 @@ function Line() {
 
   this.orientation = 0;
 
-  this.getCollisionPoints = function () {
-    if (this.orientation == 0 || this.orientation == 2) {
-      return [
-        [0, 0],
-        [0, 1],
-        [0, 2],
-        [0, 3],
-      ];
-    } else {
-      return [
-        [3, 3],
-      ];
-    }
-  };
-
-
-
+  // this.getCollisionPoints = function () {
+  //   if (this.orientation == 0 || this.orientation == 2) {
+  //     return [
+  //       [0, 0],
+  //       [0, 1],
+  //       [0, 2],
+  //       [0, 3],
+  //     ];
+  //   } else {
+  //     return [[3, 3]];
+  //   }
+  // };
 
   this.active = true;
 }
@@ -266,22 +238,23 @@ function S() {
 
   this.orientation = 0;
 
-  this.getCollisionPoints = function () {
-    if(this.orientation == 0 || this.orientation == 2){
-      return [[1,0],[1,1],[0,2]]
-    }
-    else{
-      return [[1,1],[2,2]]
-    }
-  };
-
-
-
-
+  // this.getCollisionPoints = function () {
+  //   if (this.orientation == 0 || this.orientation == 2) {
+  //     return [
+  //       [1, 0],
+  //       [1, 1],
+  //       [0, 2],
+  //     ];
+  //   } else {
+  //     return [
+  //       [1, 1],
+  //       [2, 2],
+  //     ];
+  //   }
+  // };
 
   this.active = true;
 }
-
 
 function Z() {
   this.location = 4;
@@ -294,22 +267,23 @@ function Z() {
 
   this.orientation = 0;
 
-  this.getCollisionPoints = function () {
-    if(this.orientation == 0 || this.orientation == 2){
-      return [[0,0],[1,1],[1,2]]
-    }
-    else{
-      return [[2,1],[1,2]]
-    }
-  };
-
-
-
-
+  // this.getCollisionPoints = function () {
+  //   if (this.orientation == 0 || this.orientation == 2) {
+  //     return [
+  //       [0, 0],
+  //       [1, 1],
+  //       [1, 2],
+  //     ];
+  //   } else {
+  //     return [
+  //       [2, 1],
+  //       [1, 2],
+  //     ];
+  //   }
+  // };
 
   this.active = true;
 }
-
 
 function Square() {
   this.location = 4;
@@ -322,16 +296,12 @@ function Square() {
 
   this.orientation = 0;
 
-  this.getCollisionPoints = function () {
-    return [
-      [1, 1],
-      [1, 2],
-    ];
-  };
-
-
-
-
+  // this.getCollisionPoints = function () {
+  //   return [
+  //     [1, 1],
+  //     [1, 2],
+  //   ];
+  // };
 
   this.active = true;
 }
@@ -347,43 +317,39 @@ function L() {
 
   this.orientation = 0;
 
-  this.getCollisionPoints = function () {
-    switch (this.orientation) {
-      case 0:
-        console.log("case 0");
-        return [
-          [0, 0],
-          [0, 1],
-          [1, 2],
-        ];
-        break;
-      case 1:
-        console.log("case 1");
-        return [
-          [2, 1],
-          [2, 2],
-        ];
-        break;
+  // this.getCollisionPoints = function () {
+  //   switch (this.orientation) {
+  //     case 0:
+  //       console.log("case 0");
+  //       return [
+  //         [0, 0],
+  //         [0, 1],
+  //         [1, 2],
+  //       ];
+  //       break;
+  //     case 1:
+  //       console.log("case 1");
+  //       return [
+  //         [2, 1],
+  //         [2, 2],
+  //       ];
+  //       break;
 
-      case 2:
-        console.log("case 2");
-        return [
-          [2, 0],
-          [2, 1],
-          [2, 2],
-        ];
-        break;
+  //     case 2:
+  //       console.log("case 2");
+  //       return [
+  //         [2, 0],
+  //         [2, 1],
+  //         [2, 2],
+  //       ];
+  //       break;
 
-      case 3:
-        console.log("case 0");
-        return [[2, 0]];
-        break;
-    }
-  };
-
-
-
-
+  //     case 3:
+  //       console.log("case 0");
+  //       return [[2, 0]];
+  //       break;
+  //   }
+  // };
 
   this.active = true;
 }
@@ -399,47 +365,45 @@ function J() {
 
   this.orientation = 0;
 
-  this.getCollisionPoints = function () {
-    switch (this.orientation) {
-      case 0:
-        console.log("case 0");
-        return [
-          [1, 0],
-          [1, 1],
-          [1, 2],
-        ];
-        break;
-      case 1:
-        console.log("case 1");
-        return [
-          [2, 1],
-          [0, 2],
-        ];
-        break;
+  // this.getCollisionPoints = function () {
+  //   switch (this.orientation) {
+  //     case 0:
+  //       console.log("case 0");
+  //       return [
+  //         [1, 0],
+  //         [1, 1],
+  //         [1, 2],
+  //       ];
+  //       break;
+  //     case 1:
+  //       console.log("case 1");
+  //       return [
+  //         [2, 1],
+  //         [0, 2],
+  //       ];
+  //       break;
 
-      case 2:
-        console.log("case 2");
-        return [
-          [1, 0],
-          [1, 1],
-          [2, 2],
-        ];
-        break;
+  //     case 2:
+  //       console.log("case 2");
+  //       return [
+  //         [1, 0],
+  //         [1, 1],
+  //         [2, 2],
+  //       ];
+  //       break;
 
-      case 3:
-        console.log("case 0");
-        return [[2, 0],[2,1]];
-        break;
-    }
-  };
-
-
-
-
+  //     case 3:
+  //       console.log("case 0");
+  //       return [
+  //         [2, 0],
+  //         [2, 1],
+  //       ];
+  //       break;
+  //   }
+  // };
 
   this.active = true;
 }
-
 
 function T() {
   this.location = 4;
@@ -452,48 +416,44 @@ function T() {
 
   this.orientation = 0;
 
-  this.getCollisionPoints = function () {
-    switch (this.orientation) {
-      case 0:
-        console.log("case 0");
-        return [
-          [0, 0],
-          [2, 1],
-          [0, 2],
-        ];
-        break;
-      case 1:
-        console.log("case 1");
-        return [
-          [1, 0],
-          [1, 1],
-          [2, 2],
-        ];
-        break;
+  // this.getCollisionPoints = function () {
+  //   switch (this.orientation) {
+  //     case 0:
+  //       console.log("case 0");
+  //       return [
+  //         [0, 0],
+  //         [2, 1],
+  //         [0, 2],
+  //       ];
+  //       break;
+  //     case 1:
+  //       console.log("case 1");
+  //       return [
+  //         [1, 0],
+  //         [1, 1],
+  //         [2, 2],
+  //       ];
+  //       break;
 
-      case 2:
-        console.log("case 2");
-        return [
-          [2, 0],
-          [2, 1],
-          [2, 2],
-        ];
-        break;
+  //     case 2:
+  //       console.log("case 2");
+  //       return [
+  //         [2, 0],
+  //         [2, 1],
+  //         [2, 2],
+  //       ];
+  //       break;
 
-      case 3:
-        console.log("case 0");
-        return [
-          [2, 0],
-          [1, 1],
-          [1, 2],
-        ];
-        break;
-    }
-  };
-
-
-
-
+  //     case 3:
+  //       console.log("case 0");
+  //       return [
+  //         [2, 0],
+  //         [1, 1],
+  //         [1, 2],
+  //       ];
+  //       break;
+  //   }
+  // };
 
   this.active = true;
 }
@@ -507,8 +467,7 @@ rotate = (currObj) => {
     currObj.points = currObj.points[0].map((val, index) =>
       currObj.points.map((row) => row[index]).reverse()
     );
-    addShapeColors(currObj);
-    // currObj.addColors();
+    updateColors(currObj);
   }
 };
 
@@ -540,34 +499,26 @@ removeRow = (row) => {
       grid[z] = grid[z - 10];
     }
   }
-  console.log(`new grid is `);
+
   printGrid();
 
   addColorHandler();
-
-  // setTimeout(alert("4 seconds"), 4000);
 };
 
 // Scan from left to right, bottom to top
 // If any given row contains all 10 elements, pop the row and move everything above it down by one
 checkCompleteRows = (currObj) => {
-  console.log("checking complete rows");
   let x = 90;
   while (x >= 0) {
-    console.log(`x is ${x}`);
     let rr = true;
     for (z = x; z < x + 10; z++) {
-      //   console.log(`checking row ${x} and element ${z} with grid[z]=${grid[z]} with currObj = ${currObj.active}`)
-
       if (grid[z] == 0) {
         rr = false;
-
         break;
       }
     }
 
     if (rr) {
-      console.log(`remove row ${x}`);
       removeRow(x);
       x = 90;
     } else {
@@ -577,16 +528,15 @@ checkCompleteRows = (currObj) => {
 };
 
 currObj = new J();
-addShapeColors(currObj);
-// currObj.addColors();
-
-const container = document.querySelectorAll(".container");
+updateColors(currObj);
 
 let count = 0;
 
 const interval = 200;
 
 setInterval(() => {
+  getBottomCollisionPoints(currObj);
+
   checkCollisionPoints(currObj);
 
   if (currObj.active) {
@@ -595,7 +545,6 @@ setInterval(() => {
     checkCompleteRows(currObj);
     currObj = new J();
   }
-  // printGrid();
 
   document.onkeydown = checkKey;
 
@@ -613,4 +562,3 @@ setInterval(() => {
     }
   }
 }, interval);
-
